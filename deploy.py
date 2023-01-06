ee2005cf2c4dc79399f10d659d6e5d8d99640973
Gary <wklu99@gmail.com>
2023-01-06 11:44:46 +0200

    Add mainnet deployment option

--- deploy.py
+++ deploy.py
@@ -1,4 +1,5 @@
 from os import getenv
+import sys
 
 from algosdk.atomic_transaction_composer import (
     AccountTransactionSigner,
@@ -9,36 +10,46 @@ from dotenv import load_dotenv
 
 from contracts.aarons_kit import AaronsKit
 
-# Current contract details in testnet (already deployed):
-# * App ID = 151578019
-# * Address = ZH6QHCFO4UKUHDKFMTJDAQDMENWOFRKAKQCOC4RWBE54MJKCOBXCPO6OHE
-# * Tx ID = YVTD7FVNT7TMED6RZ75QQLPVU4M2MBAJT7CXYSJSBFNSFGN6QEVQ
-
 load_dotenv(".env")
 
-# Deploying to testnet only for now
-algod_url = getenv("ALGOD_URL_TESTNET")
 algod_token = getenv("NODE_API_KEY")
 headers = {
     "X-API-Key": algod_token,
 }
+algod_client = None
 
-algod_client = algod.AlgodClient(algod_token, algod_url, headers)
 
-app_client = client.ApplicationClient(
-    client=algod_client,
-    app=AaronsKit(version=7),
-    signer=AccountTransactionSigner(getenv("DEPLOYMENT_PRIVATE")),
-)
-
-app_id, app_addr, app_txid = app_client.create()
-print(
-    """
-    Deployed contract with:
-    * App ID = {}
-    * Address = {}
-    * Tx ID = {}
-    """.format(
-        app_id, app_addr, app_txid
+def deploy(algod_client):
+    app_client = client.ApplicationClient(
+        client=algod_client,
+        app=AaronsKit(version=7),
+        signer=AccountTransactionSigner(getenv("DEPLOYMENT_PRIVATE")),
     )
-)
+
+    app_id, app_addr, app_txid = app_client.create()
+
+    print(
+        """
+        Deployed contract with:
+        * App ID = {}
+        * Address = {}
+        * Tx ID = {}
+        """.format(
+            app_id, app_addr, app_txid
+        )
+    )
+
+
+if __name__ == "__main__":
+    args = sys.argv
+
+    if len(args) > 1 and args[1] == "mainnet":
+        algod_url = getenv("ALGOD_URL_MAINNET")
+        print("Deploying to mainnet...")
+    else:
+        print("Deploying to testnet...")
+        algod_url = getenv("ALGOD_URL_TESTNET")
+
+    algod_client = algod.AlgodClient(algod_token, algod_url, headers)
+
+    deploy(algod_client)
